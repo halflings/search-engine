a5b84d170ee35b41423b07a885ed67bf7c3bb8a1
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2015-02-14 12:25:12 +0100

    Separated indexing logic from command parsing

--- indexer.py
+++ indexer.py
@@ -3,6 +3,14 @@ import os
 
 from indexing import tokenize_file
 
+def index_dir(directory, index):
+    for filename in os.listdir(directory):
+        index[filename] = set()
+        print("Indexing '{}'".format(filename))
+        file_path = os.path.join(directory, filename)
+        for i, token in enumerate(tokenize_file(file_path, encoding='latin-1')):
+            index[filename].add(token)
+
 if __name__ == '__main__':
     # Command-line arguments parsing
     parser = argparse.ArgumentParser(description='Index the given directories.')
@@ -13,9 +21,5 @@ if __name__ == '__main__':
     # Opening files and indexing them. WIP.
     index = dict()
     for directory in args.dirs:
-        for filename in os.listdir(directory):
-            index[filename] = set()
-            print "Indexing '{}'".format(filename)
-            file_path = os.path.join(directory, filename)
-            for token in tokenize_file(file_path, encoding='latin-1'):
-                index[filename].add(token)
+        index_dir(directory, index)
+    print("Finished indexing!")
